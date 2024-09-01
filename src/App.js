58206080f77e2d147f35aaef16a8e187512ec6b8
shakil-ahmmed-se <mdshakilahmmed8906@gmail.com>
2024-09-01 11:44:01 +0600

    team bulding site add

--- src/App.js
+++ src/App.js
@@ -1,23 +1,47 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
+import { useEffect } from 'react';
+import User from './components/User/User';
+import data from './data/data.json';
 
 function App() {
+  const [users, setUsers] = useState([]);
+  const [team, setTeam] = useState([]);
+  const [clubs, setClubs] = useState([]);
+ 
+  useEffect( () => {
+    setClubs(data);
+  } , [])
+
+  useEffect( () =>{
+    fetch('https://randomuser.me/api/?results=15')
+    .then(res =>  res.json())
+    .then(data => setUsers(data.results))
+  }, []);
+
+  const addMember = (name) => {
+    const exists = team.find(m => m === name);
+    if(!exists) {
+      setTeam([...team, name]);
+    }
+  }
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+        <h1>Team Builder</h1>
+        {/* <ul>
+          {
+            clubs.map(club => <li>{club.name}</li>)
+          }
+        </ul> */}
+        <ul>
+          {
+            team.map( (m, idx) => <li key={idx}>{m}</li>)
+          }
+        </ul>
+        {
+          users.map(user => <User user={user} addMember={addMember} key={user.phone}></User>)
+        }
     </div>
   );
 }
